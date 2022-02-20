b1ad8db9d3603b9e098526696d4cc97f228c6d7e
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-02-19 22:23:13 -0500

    Setup and start ingess-nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,4 +5,9 @@ const app = express();
 
 app.use(json());
 
+// routes
+app.get('/api/users/currentuser', (req, res, next) => {
+  res.send('Hi there');
+});
+
 app.listen(3000, () => console.log('Listening on port 3000'));
