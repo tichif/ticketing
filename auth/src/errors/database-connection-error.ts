9eba9f9ad89d98a17a29bb3aefb15eb85779d3b9
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-02-20 21:26:06 -0500

    Move logic errors into subclassess

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,7 +1,13 @@
 export class DatabaseConnectionError extends Error {
   reason = 'Error connecting to database.';
+  statusCode = 500;
+
   constructor() {
     super();
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
