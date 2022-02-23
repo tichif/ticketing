7cb316f9a6fe6a4b51e76aff5b36c023c26d33da
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-02-22 22:06:18 -0500

    Begin with sign in flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import { body, validationResult } from 'express-validator';
+
+import { Password } from '../utils/password';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res, next) => {
-  res.send('hi there');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Password is required.'),
+  ],
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export { router as signInRouter };
