86b982d394a188f46e06a600991a5d648a4044cd
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-02-23 08:42:41 -0500

    Sign in user

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 import { Password } from '../utils/password';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -12,11 +15,44 @@ router.post(
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().notEmpty().withMessage('Password is required.'),
   ],
-  (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const matchedPasswords = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!matchedPasswords) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // generate JWT token
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // store it on the session object
+    // req.session.jwt = jwt (JS mode)
+    req.session = {
+      jwt: userJwt,
+    }; // (typescript mode)
+
+    res.status(200).send(existingUser);
   }
 );
 
