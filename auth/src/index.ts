899f9ef0419312ddb799ef77cf3e856470c4ff0a
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-02-23 21:21:07 -0500

    Refractor index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'express';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signOutRouter } from './routes/signout';
-import { signUpRouter } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-
-// middleware
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false, // disable encryption
-    secure: true,
-  })
-);
-
-// Routes
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('*', () => {
-  throw new NotFoundError();
-});
-
-// Handle error
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
