31146b69f1540e3e645e8e01fa2b570bae0cf08d
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-03-01 13:32:05 -0500

    Add content to the Landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You're signed.</h1>
+  ) : (
+    <h1>You are not signed in.</h1>
+  );
 };
 
 export const getServerSideProps = async (ctx) => {
