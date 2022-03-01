e9d0c2bb66d91b7b4392d001def3606b12b8a131
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-03-01 12:53:47 -0500

    Fetch data over SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,45 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
+
+export const getServerSideProps = async (ctx) => {
+  // http://ingress-nginx.ingress-nginx.svc.cluster.local
+  // lyen sa ap pemet ou kominike ak yon lot sevis
+  // le wap itilize SSR
+  const { data } = await axios.get(
+    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current-user',
+    {
+      headers: {
+        Host: 'ticketing.dev',
+      },
+    }
+  );
+  return {
+    props: {
+      currentUser: data.currentUser,
+    },
+  };
+};
+
+// LandingPage.getInitialProps = async () => {
+//   if (typeof window === 'undefined') {
+//     const { data } = await axios.get(
+//       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current-user',
+//       {
+//         headers: {
+//           Host: 'ticketing.dev',
+//         },
+//       }
+//     );
+//     return data;
+//   } else {
+//     const { data } = await axios.get('/api/users/current-user');
+
+//     return data;
+//   }
+// };
+
+export default LandingPage;
