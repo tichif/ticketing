da2b37551943db6d01be78527c59bb3d494c0d3a
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-03-01 14:04:12 -0500

    Create the Sign in page

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+import useRequest from '../../hooks/useRequest';
+
+const SigninPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const router = useRouter();
+
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    body: { email, password },
+    onSuccess: () => router.push('/'),
+  });
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <h1>Sign In</h1>
+      <div className='form-group'>
+        <label htmlFor='email'>Email Address</label>
+        <input
+          type='email'
+          className='form-control'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className='form-group'>
+        <label htmlFor='Password'>Password</label>
+        <input
+          type='password'
+          className='form-control'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button type='submit' className='btn btn-primary mt-3'>
+        Sign In
+      </button>
+    </form>
+  );
+};
+
+export default SigninPage;
