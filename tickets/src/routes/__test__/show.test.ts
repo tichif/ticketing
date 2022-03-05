8175ae7319b49cb0151bf3deb4e818cec3c7a251
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-03-05 00:16:43 -0500

    Write test for get single ticket

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,28 @@
+import request from 'supertest';
+
+import { app } from '../../app';
+import { signin } from '../../test/authHelper';
+
+it('returns 404 if the ticket is not found', async () => {
+  await request(app).get('/api/tickets/szcascAS').send().expect(404);
+});
+
+it('returns the ticket if the ticket is found', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      title: 'Title',
+      price: 20,
+      userId: '1234',
+    })
+    .expect(201);
+
+  const responseTicket = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send()
+    .expect(200);
+
+  expect(responseTicket.body.title).toEqual('Title');
+  expect(responseTicket.body.price).toEqual(20);
+});
